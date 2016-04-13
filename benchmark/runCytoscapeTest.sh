36fbba20d52d56bf78ba8e393f7706e989bb6802
Daniel Kristiyanto <danielkr@uw.edu>
2016-04-13 01:04:54 -0700

    elapsed

--- benchmark/runCytoscapeTest.sh
+++ benchmark/runCytoscapeTest.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 START=$(date +%s);
-/root/cytoscape-unix-3.3.0/cytoscape.sh --rest &
+/root/cytoscape-unix-3.3.0/cytoscape.sh -R 1234 &
 Rscript /root/testBMA.R
 END=$(date +%s);
-echo $((END-START)) | awk '{print int($1/60)":"int($1%60)}'
+echo "START TIME:" $START "END TIME:" $END
+echo $((END-START)) | awk '{print "Elapsed Time = " int($1/60)":"int($1%60)}'
