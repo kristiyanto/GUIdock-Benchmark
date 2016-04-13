0673173b349ca8c50d4596b5cc3caac8ec21cd4a
Daniel Kristiyanto <danielkr@uw.edu>
2016-04-12 22:44:47 -0700

    bench

--- benchmark/runCytoscapeTest.sh
+++ benchmark/runCytoscapeTest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 START=$(date +%s);
-/root/cytoscape-unix-3.2.1/Cytoscape &
+/root/cytoscape-3.3.0/Cytoscape.sh &
 Rscript /root/testBMA.R
 END=$(date +%s);
 echo $((END-START)) | awk '{print int($1/60)":"int($1%60)}'
